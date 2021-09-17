7c4b7c4bd2131b74fa3f5e08616cd79214e5fbbe
Boho_wu <boho_wu@perfectcorp.com>
2021-09-17 13:52:44 +0800

    Change in_app_purchase_android dependency

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/bohowu/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/bohowu/Desktop/plugins-master/packages/in_app_purchase/in_app_purchase/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
